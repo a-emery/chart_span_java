bcaa2083bfee270f1094aeb4bc6599ce41b5e4e2
Aaron Emery <aaronemery@Emery-Air.att.net>
2016-06-19 18:25:12 -0400

    Removed unnecessary number variable and moved calculation of ipArray length out of for loop

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ function convert() {
     var ipArray = ip.split('.').reverse();  // Split the ip address into arrays in reverse order to make it easier to add zeros later
     var binaryArray = []; // Create an array to story the array of binary chunks
     var answer = 0;
-    for(i = 0; i < ipArray.length; i++) { // Go into each chunk of the ipAddress
-        var number = ipArray[i]; // Store the value of a chunk to a variable that can be manipulated
+    var ipArrayLength = ipArray.length; // Slightly faster than calculating everytime for loop is hit
+    for(i = 0; i < ipArrayLength; i++) { // Go into each chunk of the ipAddress
         binaryArray[i] = []; // Create an index in the binaryArray to store data about this chunk
         for(j = 7; j >= 0; j--) {
-            if(number >= Math.pow(2, j)) { // If number is larger than 2^j, add a 1 to that chunks index of the binaryArray and subtract 2^j from number
+            if(ipArray[i] >= Math.pow(2, j)) { // If ipArray[i] is larger than 2^j, add a 1 to that chunks index of the binaryArray and subtract 2^j from ipArray[i]
                 binaryArray[i].push(1);
-                number = number - Math.pow(2, j);
-            } else { // Otherwise, add a 0 and keep numer as it
+                ipArray[i] -= Math.pow(2, j);
+            } else { // Otherwise, add a 0 and keep ipArray[i] as it
                 binaryArray[i].push(0);
             }
         }
@@ -31,7 +31,8 @@ function convertEasy() {
     var ip = document.getElementById('ipInput2').value;
     var ipArray = ip.split('.').reverse();
     var answer = 0;
-    for(i = 0; i < ipArray.length; i++) {
+    var ipArrayLength = ipArray.length; // Slightly faster than calculating everytime for loop is hit
+    for(i = 0; i < ipArrayLength; i++) {
         ipArray[i] = Number(ipArray[i]).toString(2);
         for(k = 8 * i; k > 0; k--) {
             ipArray[i] += 0;
